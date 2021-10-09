5ddde8603c58d7e6be77bb8fed3b2ac981255de0
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-10-09 09:14:33 +0700

    Update hasil-pencarian.blade.php

--- resources/views/livewire/wiki/hasil-pencarian.blade.php
+++ resources/views/livewire/wiki/hasil-pencarian.blade.php
@@ -24,29 +24,36 @@
             <div class="alert alert-default-info info-hasil-text">
                 <small>Hasil pencarian yang ditemukan total <strong>{{!empty($tanaman) ? $tanaman->count() : 0}}</strong> data tanaman untuk keyword : <strong>{{$keyword}}</strong></small>
             </div>
-            @forelse($tanaman as $tanaman_detail)
-                <div class="card shadow-none border-light border mb-2">
-                    <div class="card-body">
-                        <div class="card-title">
-                            <h5 class="title-tanaman">Jeruk (Latin: Oranges)</h5>
-                        </div>
-                        <div class="card-text diskripsi-singkat-tanaman">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque aut beatae dolorem ea earum eius error id iste laboriosam magni obcaecati officia, pariatur quam quibusdam quod temporibus veniam voluptatem.</p>
-                        </div>
-                        <div class="card-text">
-                            <div class="meta-artikel">
-                                <div class="badge badge-light p-1">12 Agustus 2021</div>
-                                <div class="badge badge-light p-1">Ditinjau oleh: Abdul Aziz, SpOG</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @empty
+            @if(empty($tanaman))
                 <div class="alert alert-secondary">
                     Mohon maaf tanaman yang anda cari belum tersedia di database kami, apakah mau menambahkan tentang tanaman ini di data kami?
                     <a href="#">Klik Disini</a>
                 </div>
-            @endforelse
+            @else
+                @forelse($tanaman as $tanaman_detail)
+                    <div class="card shadow-none border-light border mb-2">
+                        <div class="card-body">
+                            <div class="card-title">
+                                <h5 class="title-tanaman">Jeruk (Latin: Oranges)</h5>
+                            </div>
+                            <div class="card-text diskripsi-singkat-tanaman">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque aut beatae dolorem ea earum eius error id iste laboriosam magni obcaecati officia, pariatur quam quibusdam quod temporibus veniam voluptatem.</p>
+                            </div>
+                            <div class="card-text">
+                                <div class="meta-artikel">
+                                    <div class="badge badge-light p-1">12 Agustus 2021</div>
+                                    <div class="badge badge-light p-1">Ditinjau oleh: Abdul Aziz, SpOG</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="alert alert-secondary">
+                        Mohon maaf tanaman yang anda cari belum tersedia di database kami, apakah mau menambahkan tentang tanaman ini di data kami?
+                        <a href="#">Klik Disini</a>
+                    </div>
+                @endforelse
+            @endif
 
         </div>
     </div>
