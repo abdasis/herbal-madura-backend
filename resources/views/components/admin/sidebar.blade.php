01a8e398ff1d3d89f49f7ec09cd25d6af245a891
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-10-13 21:06:46 +0700

    ganti template

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-light-orange elevation-1">
+<aside class="main-sidebar sidebar-light-orange ">
     <!-- Brand Logo -->
     <a href="{{route('dashboard')}}" class="brand-link border-bottom border-light">
         <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-1" style="opacity: .8">
@@ -17,21 +17,10 @@
             </div>
         </div>
 
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar border-0" type="search" placeholder="Keyword" aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar border-0">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
-            </div>
-        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-flat nav-legacy " data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -21,37 +21,33 @@ Route::get('/', \App\Http\Livewire\Beranda::class)->name('beranda');
 Route::get('/tanaman/{slug}', \App\Http\Livewire\Tanaman\Baca::class)->name('tanaman.baca');
 Route::get('hasil-pencarian/{keyword}', \App\Http\Livewire\Wiki\HasilPencarian::class)->name('wiki.hasil-pencarian');
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function (){
-   Route::group(['prefix' => 'product'], function (){
-        Route::get('semua', \App\Http\Livewire\Product\Semua::class)->name('product.semua');
-        Route::get('tambah', \App\Http\Livewire\Product\Tambah::class)->name('product.tambah');
-        Route::get('sunting/{slug}', \App\Http\Livewire\Product\Sunting::class)->name('product.sunting');
-        Route::get('detail/{slug}', \App\Http\Livewire\Product\Detail::class)->name('product.detail');
-   });
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+     Route::group(['prefix' => 'product'], function () {
+          Route::get('semua', \App\Http\Livewire\Product\Semua::class)->name('product.semua');
+          Route::get('tambah', \App\Http\Livewire\Product\Tambah::class)->name('product.tambah');
+          Route::get('sunting/{slug}', \App\Http\Livewire\Product\Sunting::class)->name('product.sunting');
+          Route::get('detail/{slug}', \App\Http\Livewire\Product\Detail::class)->name('product.detail');
+     });
 
-   Route::group(['prefix' => 'tanaman'], function (){
-        Route::get('semua', \App\Http\Livewire\Tanaman\Semua::class)->name('tanaman.semua');
-        Route::get('tambah', \App\Http\Livewire\Tanaman\Tambah::class)->name('tanaman.tambah');
-        Route::get('sunting/{slug}', \App\Http\Livewire\Tanaman\Sunting::class)->name('tanaman.sunting');
-        Route::get('detail/{slug}', \App\Http\Livewire\Tanaman\Detail::class)->name('tanaman.detail');
-   });
+     Route::group(['prefix' => 'tanaman'], function () {
+          Route::get('semua', \App\Http\Livewire\Tanaman\Semua::class)->name('tanaman.semua');
+          Route::get('tambah', \App\Http\Livewire\Tanaman\Tambah::class)->name('tanaman.tambah');
+          Route::get('sunting/{slug}', \App\Http\Livewire\Tanaman\Sunting::class)->name('tanaman.sunting');
+          Route::get('detail/{slug}', \App\Http\Livewire\Tanaman\Detail::class)->name('tanaman.detail');
+     });
 
-   Route::group(['prefix' => 'pengguna'], function (){
-        Route::get('semua', Semua::class)->name('pengguna.semua');
-        Route::get('tambah', Tambah::class)->name('pengguna.tambah');
-        Route::get('sunting/{slug}', Sunting::class)->name('pengguna.sunting');
-        Route::get('detail/{slug}', Detail::class)->name('pengguna.detail');
-   });
+     Route::group(['prefix' => 'pengguna'], function () {
+          Route::get('semua', Semua::class)->name('pengguna.semua');
+          Route::get('tambah', Tambah::class)->name('pengguna.tambah');
+          Route::get('sunting/{slug}', Sunting::class)->name('pengguna.sunting');
+          Route::get('detail/{slug}', Detail::class)->name('pengguna.detail');
+     });
 
-   Route::group(['prefix' => 'statistik'], function (){
-       Route::get('semua', App\Http\Livewire\Statistik\Semua::class)->name('statistik.semua');
-   });
-
-   Route::get('test', function(){
-        return view('dashboard')
-   })
+     Route::group(['prefix' => 'statistik'], function () {
+          Route::get('semua', App\Http\Livewire\Statistik\Semua::class)->name('statistik.semua');
+     });
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+     return view('dashboard');
 })->name('dashboard');
