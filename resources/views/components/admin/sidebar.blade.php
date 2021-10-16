88d02c8a4a309bafe2d9fd9ab60aa594bc3014bb
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-10-16 18:10:14 +0700

    Perbaiki header pada sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-light-orange ">
     <!-- Brand Logo -->
     <a href="{{route('dashboard')}}" class="brand-link border-bottom border-light">
-        <img src="{{asset('dist/img/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-1" style="opacity: .8">
-        <span class="brand-text font-weight-light">HerbalQoe</span>
+        <img src="{{asset('dist/img/logo.png')}}" alt="AdminLTE Logo" class="brand-image" style="opacity: .8">
+        <span class="brand-text font-weight-light text-white">.</span>
     </a>
 
     <!-- Sidebar -->
