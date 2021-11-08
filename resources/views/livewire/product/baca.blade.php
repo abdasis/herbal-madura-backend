4df5782b17275e3df457602f18d445065937ca4d
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-11-08 11:45:38 +0700

    Update baca.blade.php

--- resources/views/livewire/product/baca.blade.php
+++ resources/views/livewire/product/baca.blade.php
@@ -1,15 +1,27 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
     <div class="blur"></div>
-    <div class="row justify-content-center">
+    <div class="row justify-content-center" style="min-height: 100vh">
         <div class="col-md-7">
-            <div class="card">
-                <div class="row p-2">
+            <div class="card p-3">
+                <div class="row">
                     <div class="col-md-6">
                         <img src="{{asset('upload/' . '/' . $produk->gambar_produk)}}" class="img-fluid rounded" alt="">
                     </div>
                     <div class="col-md-6">
-
+                        <div class="title-produk">
+                            <h4 class="font-weight-bolder font-quicksand text-orange">{{$produk->nama_produk}}</h4>
+                            <small class="text-muted">Dipublikasikan : {{$produk->created_at}}</small>
+                        </div>
+                        <div class="deskripsi-produk">
+                            {!! $produk->deskripsi !!}
+                        </div>
+                        <hr>
+                        <small class="text-muted">Produsen Jamu</small>
+                        <p class="nama-penulis">{{$produk->produsen}}</p>
+                        @if(!empty($produk->link_produk))
+                            <button class="btn btn-sm btn-warning">Kunjungi Toko Online Produk</button>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -21,6 +33,36 @@
 
 @push('css')
     <style>
+        .title-produk{
+            margin-bottom: 30px;
+        }
+        .font-quicksand{
+            font-family: 'Quicksand', sans-serif;
+
+        }
+
+        .deskripsi-produk h2{
+            font-size: 24px;
+            font-family: 'Quicksand', sans-serif;
+            font-weight: bold;
+            margin-bottom: 10px;
+            margin-top: 10px;
+
+        }
+
+        .deskripsi-produk h3{
+            font-size: 18px;
+            font-family: 'Quicksand', sans-serif;
+            font-weight: bold;
+        }
+        .deskripsi-produk p{
+            font-size: 16px;
+            font-family: 'Quicksand', sans-serif;
+            font-weight: 500;
+            line-height: 20px;
+            padding: 0;
+            margin: 0;
+        }
         .riwayat-revisi h5{
             font-weight: bold;
             font-family: 'Quicksand', sans-serif;
