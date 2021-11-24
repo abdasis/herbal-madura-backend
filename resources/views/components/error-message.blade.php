e41103def9a4447c8dcecdf76b131959f12b3249
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2021-11-24 14:27:15 +0700

    update git ignore

--- resources/views/components/error-message.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
-    @error($error)
-        <small class="text-danger">{{$message}}</small>
-    @enderror
-</div>
