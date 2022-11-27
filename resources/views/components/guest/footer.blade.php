f40a19f937f0c3972947f911f550f69218896f30
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-11-28 06:27:49 +0700

    redesign: component footer

--- resources/views/components/guest/footer.blade.php
+++ resources/views/components/guest/footer.blade.php
@@ -1,34 +1,40 @@
 <div>
     <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-    <footer class="py-5 border-top border-light">
-        <div class="container">
-            <div class="row justify-content-between hstack">
-                <div class="col-md-5">
-                    <div class="widget-footer">
-                        <div class="widget-footer-title">
-                            <h3>Planty</h3>
-                            <p>
-                                Sebuah platform yang memberikan informasi tentang tanaman herbal yang ada di nusantara, yang bisa di baca secara gratis dimana dan kapan saja.
-                            </p>
+    <div class="footer-container">
+        <footer class="py-5 border-top border-light">
+            <div class="container">
+                <div class="row justify-content-between hstack">
+                    <div class="col-md-5">
+                        <div class="widget-footer">
+                            <div class="widget-footer-title">
+                                <h3>Planty</h3>
+                                <p>
+                                    Sebuah platform yang memberikan informasi tentang tanaman herbal yang ada di nusantara, yang bisa di baca secara gratis dimana dan kapan saja.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="widget-footer">
-                        <div class="widget-footer title">
-                            <h5>Social</h5>
+                    <div class="col-md-2">
+                        <div class="widget-footer">
+                            <div class="widget-footer title">
+                                <h5>Social</h5>
+                            </div>
+                            <ul class="list-unstyled p-0 m-0">
+                                <li>Facebook</li>
+                                <li>Twitter</li>
+                                <li>Instagram</li>
+                                <li>YouTube</li>
+                            </ul>
                         </div>
-                        <ul class="list-unstyled p-0 m-0">
-                            <li>Facebook</li>
-                            <li>Twitter</li>
-                            <li>Instagram</li>
-                            <li>YouTube</li>
-                        </ul>
                     </div>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-2"></div>
                 </div>
-                <div class="col-md-2"></div>
-                <div class="col-md-2"></div>
             </div>
-        </div>
-    </footer>
+        </footer>
+        <footer class="text-center footer-bottom border-top border-light bg-white py-4 ">
+            <p class="py-0 my-0">Planty adalah aplikasi yang dibuat Abdul Aziz untuk memenuhi kelulusan Skripsi</p>
+            <p class="py-0 my-0"><strong>©️ 2021 - {{now()->format('Y')}} Allright Reserved</strong></p>
+        </footer>
+    </div>
 </div>
