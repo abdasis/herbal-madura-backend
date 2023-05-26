7194ef54ac545745506fd03e926a277150189998
Abd Asis <34927165+abdasis@users.noreply.github.com>
2023-05-27 06:19:38 +0700

    Perbaikan pada fitur validasi dan edit tanaman di kontributor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
     <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link
         href="https://fonts.googleapis.com/css2?family=Calistoga&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Karla:ital,wght@0,500;0,600;0,700;0,800;1,500;1,600;1,700;1,800&family=Pacifico&family=Vesper+Libre:wght@400;500;700;900&display=swap"
         rel="stylesheet">
